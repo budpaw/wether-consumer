5e6b83cd170b069b60e13d656fa848dc15bb5e74
Pawel Budzyn <pawel-1989@wp.pl>
2023-04-27 21:58:12 +0200

    Preparing class
    Prepare class for new api client

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import requests
+import os
+from config import load_config
 
-API_TOKEN='<YOUR_TOKEN>'
 
 
-url  = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?last30days&key={API_TOKEN}&contentType=json"
-
-data = requests.get(url)
-
-print(data.content)
+class WeatherConsumer:
+    __api_key=None
+    host=None
+    
+    def __init__(self):
+        cfg = load_config()
+        self.__api_key = cfg['api_key']
+        self.host = cfg['host']
+        
+    def __read(self):
+        url  = f"{self.host}/VisualCrossingWebServices/rest/services/timeline/London,UK?last30days&key={self.__api_key}&contentType=json"
+        return requests.get(url)
+    
+    
+    def _transform(self):
+        pass
+    
+    
+    def last_30days(self):
+        data = self.__read()
+        return data.json()
+        
+w = WeatherConsumer()
+print(w.last_30days())
