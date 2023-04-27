e26bdb51f75acaf7a2ef2105f8de4e5a0c6d7019
Pawel Budzyn <pawel-1989@wp.pl>
2023-04-28 00:39:30 +0200

    add debuging

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests
 import os
 from config import load_config
 import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 class WeatherConsumer:
     __api_key=None
@@ -9,47 +15,70 @@ class WeatherConsumer:
     countries = None
 
     def __init__(self):
+        logging.debug("Init WeatherConsumer")
         cfg = load_config()
+        logging.debug("Load configuration")
         self.__api_key = cfg['api_key']
         self.host = cfg['host']
         self.countries = cfg['countries']
         
-    def __read(self, location):        
-        url  = f"{self.host}/VisualCrossingWebServices/rest/services/timeline/{location}?last30days&key={self.__api_key}&contentType=json&include=days&elements=datetime,temp,windspeed"
-        result = requests.get(url).json()
-        return result
-    
-    
+    def __read(self, location):
+        logging.debug(f"API Request for: {location}")        
+        url  = f"{self.host}/VisualCrossingWebServices/rest/services/timeline/{location}?last30days&key={self.__api_key}&contentType=json&include=days&elements=datetime,temp,windspeed"        
+        result = requests.get(url)        
+        if result.status_code == 200:
+            logging.debug(f"API Request result: SUCCESS")   
+            return result.json()
+        else:
+            logging.error(f"API Request result: FAILED")
+            logging.error(f"   Status code: {result.status_code}") 
+            logging.error(f"   Message: {result.content.decode().replace(self.__api_key)}") 
+            return {}
+        
     def _transform(self, data):
-        df = pd.json_normalize(data).explode('days')        
+        logging.debug("Transformation start")
+        logging.debug(f"Checking data for future transformS")   
+        war = any(not x for x in data)
+        if war:
+            logging.error(f"One or more record is empty please check logs above")
+            return pd.DataFrame(columns=['city','temp_avg', 'temp_median', 'wind_avg', 'wind_median'])
+        
+        logging.debug(f"Creating DataFrame")   
+        df = pd.json_normalize(data).explode('days')
+                
+        logging.debug(f"Extract data from subnodes")  
         df['datetime'] = df.apply(lambda x: x['days']['datetime'], axis = 1)
         df['temp'] = df.apply(lambda x: x['days']['temp'], axis = 1)
         df['windspeed'] = df.apply(lambda x: x['days']['windspeed'], axis = 1)
         
-        df = df.drop(['days', 'queryCost', 'latitude', 'longitude','resolvedAddress','timezone', 'tzoffset'], axis = 1)
-        df = df.groupby('address').agg({'temp': ['mean', 'median'],'windspeed': ['mean', 'median']})
+        logging.debug(f"Drop unusful columns")
+        df = df.drop(['days', 'queryCost', 'latitude', 'longitude', 'resolvedAddress','timezone', 'tzoffset'], axis = 1)
+        df = df.groupby('address').agg({'temp': ['mean', 'median'], 'windspeed': ['mean', 'median']})
         
+        logging.debug(f"Columns rename")
         df.columns = ['temp_avg', 'temp_median', 'wind_avg', 'wind_median']
+        
+        logging.debug(f"Update index")
         df = df.reset_index(drop=False).rename(columns={'address':"city"}) 
-
-
         return df
 
-    def preety_print(self, df):
-        df['temp_avg'] = df['temp_avg'].apply(lambda x: "{:.2f}".format(x))
-        df['temp_median'] = df['temp_median'].apply(lambda x: "{:.2f}".format(x))
-        df['wind_avg'] = df['wind_avg'].apply(lambda x: "{:.2f}".format(x))
-        df['wind_median'] = df['wind_median'].apply(lambda x: "{:.2f}".format(x))
-        
+    def preety_print(self, df, output_format='df'):
+        for column in ['temp_avg', 'temp_median', 'wind_avg', 'wind_median']:
+            df[column] = df[column].apply(lambda x: "{:.2f}".format(x))
+
         max_len = df.apply(lambda x: x.str.len()).max()
         df = df.apply(lambda x: x.str.ljust(max_len[x.name]))
-        df = df.to_string(justify='left')
-        return df
+        df = df.to_string(justify='left', index=False)
+        
+        print(df)
     
     def last_30days(self):
-        raw_data = [self.__read(','.join((country['city'].lower(),country['country'].upper()))) for country in self.countries]
+        def format_country(country):            
+            return ','.join((country['city'].lower(),country['country'].upper()))        
+        raw_data = [self.__read(format_country(country)) for country in self.countries]
         result = self._transform(raw_data)
         return self.preety_print(result)
+    
         
 w = WeatherConsumer()
-print(w.last_30days())
+w.last_30days()
